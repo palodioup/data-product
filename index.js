1264de7da8e39b8a23781a9071886693981a68ec
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-11-02 11:15:24 +0100

    Removed some javascript

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 // Variables: Assignments
 
 const body = document.getElementById("main");
-const randomPassword =
-  Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10);
-
-const stringifiedPassword =
-  randomPassword +
-  "a" +
-  (randomPassword +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 5)) +
-  "-" +
-  (randomPassword +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 5)) +
-  "__" +
-  (randomPassword +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 5)) +
-  "b" +
-  (randomPassword +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 5)) +
-  "z" +
-  (randomPassword +
-    Math.floor(Math.random() * 10) +
-    Math.floor(Math.random() * 5));
-
-// Variables: Objects
 
 const credentials = {
   name: "Oladipupo Nathan Akanji",
-  password: stringifiedPassword,
+  password: 1029384756,
   email: "name@example.com",
   otherCredentials: {
     age: 9,
@@ -44,10 +17,10 @@ const credentials = {
 body.innerHTML = `
 <h1>We've created an account for you</h1>
 <p> Name: ${credentials.name}</p>
-<p> Password: ${credentials.password}</p>
+<p> Product Id: ${credentials.password}</p>
 <p> Email: ${credentials.email}</p>
 <p> Age: ${credentials.otherCredentials.age}</p>
-<p> Siblings: ${credentials.otherCredentials.siblings}</p>
+<p> Siblings${credentials.otherCredentials.siblings}</p>
 `;
 
 // runner
