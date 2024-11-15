d4315057ee90e1b4ed59d704c7ce82a2561bf836
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-11-15 17:32:00 +0100

    The average commit

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-// Variables: Assignments
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-const body = document.getElementById("main");
+function generateAverage(array) {
+  const addedArray = array.reduce((a, b) => {
+    return a + b;
+  }, 0);
+  const averageArray = addedArray / array.length;
+  console.log(averageArray);
+}
 
-const credentials = {
-  name: "Oladipupo Nathan Akanji",
-  password: 1029384756,
-  email: "name@example.com",
-  otherCredentials: {
-    age: 9,
-    siblings: 2,
+generateAverage(num);
+
+const numTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function updateArray() {
+  const descendingVar = numTwo.sort((a, b) => {
+    return b - a;
+  });
+  console.log(descendingVar);
+}
+
+updateArray();
+
+const dishes = {
+  plates: 3,
+  cups: 7,
+  cutlery: {
+    forks: 3,
+    knives: 3,
   },
 };
 
-// Making things show
+delete dishes.plates;
+console.log(dishes);
 
-body.innerHTML = `
-<h1>We've created an account for you</h1>
-<p> Name: ${credentials.name}</p>
-<p> Product Id: ${credentials.password}</p>
-<p> Email: ${credentials.email}</p>
-<p> Age: ${credentials.otherCredentials.age}</p>
-<p> Siblings${credentials.otherCredentials.siblings}</p>
-`;
+const myJSON = {
+  name: "Oladipupo",
+  age: 9,
+  class: "JSS IB",
+};
 
-// runner
+const stringify = JSON.stringify(myJSON);
+console.log(stringify);
+
+const parseJSON = '{"name": "Oladipupo", "age": 9, "class": "JSS IB"}';
+
+const parse = JSON.parse(parseJSON);
+console.log(parse);
