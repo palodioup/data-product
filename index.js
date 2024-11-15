c7f5435f2e93c0cc51920e8313274d9559c44d7b
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-11-15 18:30:04 +0100

    Added closures

--- index.js
+++ index.js
@@ -1,48 +1,99 @@
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-function generateAverage(array) {
-  const addedArray = array.reduce((a, b) => {
-    return a + b;
-  }, 0);
-  const averageArray = addedArray / array.length;
-  console.log(averageArray);
+// function generateAverage(array) {
+//   const addedArray = array.reduce((a, b) => {
+//     return a + b;
+//   }, 0);
+//   const averageArray = addedArray / array.length;
+//   console.log(averageArray);
+// }
+
+// generateAverage(num);
+
+// const numTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function updateArray() {
+//   const descendingVar = numTwo.sort((a, b) => {
+//     return b - a;
+//   });
+//   console.log(descendingVar);
+// }
+
+// updateArray();
+
+// const dishes = {
+//   plates: 3,
+//   cups: 7,
+//   cutlery: {
+//     forks: 3,
+//     knives: 3,
+//   },
+// };
+
+// delete dishes.plates;
+// console.log(dishes);
+
+// const myJSON = {
+//   name: "Oladipupo",
+//   age: 9,
+//   class: "JSS IB",
+// };
+
+// const stringify = JSON.stringify(myJSON);
+// console.log(stringify);
+
+// const parseJSON = '{"name": "Oladipupo", "age": 9, "class": "JSS IB"}';
+
+// const parse = JSON.parse(parseJSON);
+// console.log(parse);
+
+
+
+
+
+
+
+ 
+// CLOSURES I
+
+
+function apple() {
+  var fruit = "pineapple"
+  function nameFruit() {
+    console.log(fruit)
+  }
+  return nameFruit
 }
 
-generateAverage(num);
+apple()()
 
-const numTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function updateArray() {
-  const descendingVar = numTwo.sort((a, b) => {
-    return b - a;
-  });
-  console.log(descendingVar);
-}
+// CLOSURES II
 
-updateArray();
 
-const dishes = {
-  plates: 3,
-  cups: 7,
-  cutlery: {
-    forks: 3,
-    knives: 3,
-  },
-};
+const number = (() => {
+  const privateNumbers = 0
+  const changeBy = (val) => {
+    privateNumbers += val
+  }
+  return {
+    increment: () => {
+      changeBy(1)
+    },
+    decrement: () => {
+      changeBy(-1)
+    },
+    value: () => {
+      return privateNumbers
+    }
+  }
+})()
 
-delete dishes.plates;
-console.log(dishes);
-
-const myJSON = {
-  name: "Oladipupo",
-  age: 9,
-  class: "JSS IB",
-};
-
-const stringify = JSON.stringify(myJSON);
-console.log(stringify);
-
-const parseJSON = '{"name": "Oladipupo", "age": 9, "class": "JSS IB"}';
-
-const parse = JSON.parse(parseJSON);
-console.log(parse);
+console.log(number.value())
+number.increment()
+number.increment()
+number.increment()
+number.increment()
+console.log(number.value())
+number.decrement()
+console.log(number.value())
